fe31ed14f1c19a4f3df36f236f0db12f2acd9b17
vishnu244 <vishnunali244@gmail.com>
2022-04-22 19:54:12 +0530

    Perfect number program

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -9,13 +9,17 @@ Console.WriteLine("Welcome to Logical Programmiing!");
 while (true)
 {
 
-    Console.WriteLine("Choose the Program to be executed :\n1)Febinocciseries");
+    Console.WriteLine("Choose the Program to be executed :\n1)Febinocciseries\n2)PerfectNumber");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             Febinocciseries febinocci = new();
             febinocci.febinocciseries();
+            break;
+        case 2:
+            PerfectNumber perfectNumber = new ();
+            perfectNumber.perfefctnumber();
             break;
         default:
             break;
