d8295b52452a2f40b19d7240cbef70332c91c10b
vishnu244 <vishnunali244@gmail.com>
2022-04-22 15:30:08 +0530

    Febinocci series

---
+++ LogicalPrograms/Febinocciseries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Febinocciseries
+    {
+        public void febinocciseries()
+        {
+            int a = 0, b = 1, c, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write(a+ " " + b+ " ");    
+            for (i = 2; i < number; i++)   
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using LogicalPrograms;
+
 Console.WriteLine("Welcome to Logical Programmiing!");
+
+
+while (true)
+{
+
+    Console.WriteLine("Choose the Program to be executed :\n1)Febinocciseries");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Febinocciseries febinocci = new();
+            febinocci.febinocciseries();
+            break;
+        default:
+            break;
+    }
+}
