b9ac117f03d53ad5a988b0cc156dad6f554bc3ac
vishnu244 <vishnunali244@gmail.com>
2022-04-26 17:02:06 +0530

    CouponCode Program

---
+++ LogicalPrograms/couponcode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class couponcode
+    {
+        public void CouponCode()
+        {
+            Console.WriteLine("Enter number of digits :");
+            int digits = Convert.ToInt16(Console.ReadLine());
+            string coupon = string.Empty;
+            while (digits > 0)
+            {
+                int random = new Random().Next(1, 123);
+                coupon += (char)random;
+                digits -- ;
+
+            }
+            Console.WriteLine("coupon number = " + coupon );
+        }
+    }
+}
